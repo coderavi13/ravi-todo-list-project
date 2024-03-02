76f6d35af877582137c6e9a30407d4627a69fe57
coderavi <coderavi>
2024-03-02 23:24:59 +0530

    Added sample list Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ListItem from './components/ListItem/ListItem'
 
 function App() {
 
   return (
     <>
        <h2>Ravi's Todo List Project</h2>
+       <ListItem />
     </>
   )
 }
---
+++ src/components/ListItem/ListItem.tsx
@@ -0,0 +1,9 @@
+function ListItem() {
+    return (
+        <>
+        List Item 1
+        </>
+    );
+}
+
+export default ListItem;
